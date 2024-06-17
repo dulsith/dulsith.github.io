d1ea7c66bd7f6833e97a0b2de48117ad4f3098b6
dulsith <125107337+dulsith@users.noreply.github.com>
2024-06-17 09:49:57 +0530

    failed

---
+++ README.md
@@ -0,0 +1,2 @@
+# dulsith.github.io
+
---
+++ funtion.js
@@ -0,0 +1,11 @@
+var btn = document.querySelectorAll('.photo');
+btn.forEach(applyToList)
+function applyToList(item ,index,arr){
+     item.addEventListener('click',()=>{
+        item.classList.toggle('clicked');
+    });
+}
+console.log(btn);
+// btn.addEventListener('click',()=>{
+//     btn.classList.toggle('clicked');
+// });
