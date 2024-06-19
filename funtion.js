374eeaa85fcc5be00ff6ccffe5b90aed46b53eb1
dulsith <125107337+dulsith@users.noreply.github.com>
2024-06-19 11:19:42 +0530

    final final submit designed and bug fixes

--- funtion.js
+++ funtion.js
@@ -4,3 +4,11 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+document.addEventListener('DOMContentLoaded', function () {
+    var tag_id = 'particles-container';
+    var path_config_json = 'particlesjs-config.json';
+
+    window.particlesJS.load(tag_id, path_config_json, function() {
+        console.log('Particles.js configuration loaded and initialized.');
+    });
+});
